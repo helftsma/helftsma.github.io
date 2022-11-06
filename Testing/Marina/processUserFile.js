d9456910b67186102d797b3e6aa4b1ee5befdf00
2569221s <2569221s@student.gla.ac.uk>
2022-11-06 21:18:13 +0000

    Add code dynamically to pyScript tag

--- Testing/Marina/processUserFile.js
+++ Testing/Marina/processUserFile.js
@@ -1,26 +1,14 @@
+document.getElementById('upload-button').addEventListener('change', function(e) {
+  let file = document.getElementById('upload-button').files[0];
 
-const fileSelector = document.getElementById('file-input');
-fileSelector.addEventListener('change', (event) => {
-  const fileList = event.target.files[0];
-  console.log("Uploaded file");
-  readFile(fileList);
+  (async () => {
+    const fileContent = await file.text();
+
+    console.log(fileContent);
+
+    let insertPoint = document.getElementById('user-code');
+    insertPoint.innerHTML = fileContent;
+    let myPyScript = insertPoint.firstElementChild;
+    myPyScript.evaluate();
+  })();
 });
-
-function readFile(file) {
-  console.log("Reading file");
-
-  const reader = new FileReader();
-  reader.addEventListener('load', (event) => {
-    const result = event.target.result;
-    console.log(result);
-  });
-
-  reader.addEventListener('progress', (event) => {
-    if (event.loaded && event.total) {
-      const percent = (event.loaded / event.total) * 100;
-      console.log(`Progress: ${Math.round(percent)}`);
-    }
-  });
-
-  reader.readAsText(file);
-}
