b1a44e617cbdadc0c39c69080872d17c56bf2b5c
2569221s <2569221s@student.gla.ac.uk>
2022-11-07 07:36:33 +0000

    Run input code in HTML tag

--- Testing/Marina/processUserFile.js
+++ Testing/Marina/processUserFile.js
@@ -1,3 +1,37 @@
+// Global variable to store the py-script DIV
+let py_div;
+let py_output;
+function run_python(py_code) {
+        // remove the previous script tag
+        if (py_div) {
+                py_div.remove();
+        }
+
+        // Wrap the Python code (py_code) with a PyScript tag
+        // py_div.evaluate() will run the code within the <py-script> tag
+        let html_tag = `
+        <py-script output="${py_output.id}">
+        ${py_code}
+        </py-script>
+        `;
+
+        // Create the DIV to attach the py-script tag
+        let div = document.createElement("div");
+        div.innerHTML = html_tag;
+
+        py_div = div.firstElementChild;
+        document.body.appendChild(py_div);
+
+        try {
+                // This will run the Python interpreter
+                // for the code loaded into py_div
+                py_div.evaluate();
+        } catch (error) {
+                console.error("Python error:");
+                console.error(error);
+        }
+}
+
 document.getElementById('upload-button').addEventListener('change', function(e) {
   let file = document.getElementById('upload-button').files[0];
 
@@ -6,9 +40,13 @@ document.getElementById('upload-button').addEventListener('change', function(e) 
 
     console.log(fileContent);
 
-    let insertPoint = document.getElementById('user-code');
-    insertPoint.innerHTML = fileContent;
-    let myPyScript = insertPoint.firstElementChild;
-    myPyScript.evaluate();
+    // Create a DIV to store Python program print output
+    py_output = document.createElement("div");
+    py_output.id = "user-code";
+    document.body.appendChild(py_output);
+
+    run_python(fileContent);
+
   })();
 });
+
