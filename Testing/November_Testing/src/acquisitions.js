15d39e93d1f8143470f9a5b624c441a71f549496
“Alexandra <2550454m@student.gla.ac.uk>
2022-11-29 17:29:43 +0000

    Graph reads dictionary of lists

--- Testing/November_Testing/src/acquisitions.js
+++ Testing/November_Testing/src/acquisitions.js
@@ -1,34 +1,24 @@
 import { Chart } from 'chart.js/auto'
 
 (async function() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+  const data =
+    { year: [2010, 2011, 2012, 2013, 2014, 2015, 2016], count: [10, 20, 15, 25, 22, 30, 28] };
 
   new Chart(
     document.getElementById('acquisitions'),
     {
       type: 'line',
       data: {
-        labels: data.map(row => row.year),
+        labels: data['year'],
         datasets: [
           {
             label: 'Acquisitions by year',
-            data: data.map(row => row.count)
+            data: data['count']
           }
         ]
       },
       options: {
         plugins: {
-          customCanvasBackgroundColor: {
-            color: 'lightGreens',
-          }
        }
     },
     }
